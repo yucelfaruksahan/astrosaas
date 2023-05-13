64058ec486d863ca84a10387598af9736ecad6ce
Yucel Faruk Sahan <yucelfaruksahan@gmail.com>
2023-05-14 01:36:10 +0200

    robot txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import image from "@astrojs/image";
 import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://astrosaas-five.vercel.app/',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+  site: 'https://astrosaas-five.vercel.app/',
+  integrations: [tailwind(), image(), compress(), partytown(), sitemap(), robotsTxt()]
 });
